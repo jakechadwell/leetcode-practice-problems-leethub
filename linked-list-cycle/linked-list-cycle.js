85a76dd8f6e0e1ea1cf64461529e14a37c7ac407
Jake Chadwell <jake.chadwell01@gmail.com>
2021-09-14 23:53:11 -0500

    Time: 102 ms (23.06%), Space: 40.9 MB (93.17%) - LeetHub

---
+++ linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(!head) return false
+    slowPointer = head
+    fastPointer = head.next
+    while(1) {
+        if(!fastPointer || !fastPointer.next) return false
+        if(slowPointer == fastPointer || slowPointer == fastPointer.next) return true
+        slowPointer = slowPointer.next
+        fastPointer = fastPointer.next.next
+    }
+};
